f31cdf82e5ddcc704ac96348c1b7e9bf246bafa1
✨Challyfilio✨ <challyfilio4368@vip.qq.com>
2022-01-13 16:58:30 +0800

    Update summary.py

--- summary.py
+++ summary.py
@@ -1,6 +1,6 @@
-#--------------------------------------------#
+# --------------------------------------------#
 #   该部分代码用于看网络结构
-#--------------------------------------------#
+# --------------------------------------------#
 import torch
 from torchsummary import summary
 
@@ -8,6 +8,6 @@ from nets.frcnn import FasterRCNN
 
 if __name__ == "__main__":
     # 需要使用device来指定网络在GPU还是CPU运行
-    device  = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-    m       = FasterRCNN(20, backbone = 'vgg').to(device)
+    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+    m = FasterRCNN(20, backbone='vgg').to(device)
     summary(m, input_size=(3, 600, 600))
